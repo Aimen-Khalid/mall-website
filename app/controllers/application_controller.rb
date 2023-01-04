2bf6cfc251d3519c981f5d85cee722597b13608c
AimenKhalid338 <121729095+AimenKhalid338@users.noreply.github.com>
2023-01-04 13:52:46 +0500

    levis product error fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
     include Pundit::Authorization
-    private
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-    def user_not_authorized
-      flash[:alert] = "You are not authorized to perform this action."
-      redirect_back(fallback_location: root_path)
-    end
+ private
+
+ def user_not_authorized(exception)
+   policy_name = exception.policy.class.to_s.underscore
+
+   flash[:error] = t "#{policy_name}.#{exception.query}", scope: "pundit", default: :default
+   redirect_back(fallback_url: root_path)
+ end
 end
